811a4079ba07d968b8a6042278e1c64efb4523e9
tsawi <35880577+tsawi@users.noreply.github.com>
2021-05-20 20:10:46 -0400

    Add files via upload

---
+++ 1_makeWaveformsDataset.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 17:00:19 2021
+example: Parkfield repeaters::
+@author: theresasawi
+"""
+
+
+
+import h5py
+import numpy as np
+import glob
+import sys
+import obspy
+import os
+import pandas as pd
+
+sys.path.append('functions/')
+from setParams import setParams
+from generators import gen_wf_from_folder
+
+import tables
+tables.file._open_files.close_all()
+
+
+
+#%% load project variables: names and paths
+
+
+key = sys.argv[1]
+print(key)
+print(key)
+
+print(key)
+
+print(key)
+
+#example
+# key = 'Parkfield_Repeaters'
+
+# key='1'
+
+pathProj, pathCat, pathWF, network, station, channel, channel_ID, filetype, cat_columns = setParams(key)
+
+
+dataH5_name = f'data_{key}.hdf5'
+
+dataH5_path = pathProj + '/H5files/' + dataH5_name
+
+wf_cat_out = pathProj + 'wf_cat_out.csv'
+
+
+if not os.path.isdir(pathProj + '/H5files/'):
+    os.mkdir(pathProj + '/H5files/')
+
+#%% get global catalog
+
+cat = pd.read_csv(pathCat, header=None,delim_whitespace=True)
+
+
+cat.columns = cat_columns
+
+#for plotting
+cat['datetime'] = pd.to_datetime(cat[['year','month','day','hour','minute','second']])
+
+
+cat['event_ID'] = [int(evID) for evID in cat.event_ID]
+
+
+
+
+#%% get list of waveforms and sort
+
+wf_filelist = glob.glob(pathWF + '*')
+wf_filelist.sort()
+
+wf_filelist = wf_filelist
+
+wf_test = obspy.read(wf_filelist[0])
+
+lenData = len(wf_test[0].data)
+
+#%%
+
+
+gen_wf = gen_wf_from_folder(wf_filelist,key,lenData,channel_ID)
+
+
+
+if os.path.exists(dataH5_path):
+    os.remove(dataH5_path)
+evID_keep = []
+with h5py.File(dataH5_path,'a') as h5file:
+
+
+
+    global_catalog_group =  h5file.create_group("catalog/global_catalog")
+
+
+    for col in cat.columns:
+
+        if col == 'datetime':
+            global_catalog_group.create_dataset(name='datetime',data=np.array(cat['datetime'],dtype='S'))
+
+        else:
+            exec(f"global_catalog_group.create_dataset(name='{col}',data=cat.{col})")
+
+
+    waveforms_group  = h5file.create_group("waveforms")
+    station_group = h5file.create_group(f"waveforms/{station}")
+    channel_group = h5file.create_group(f"waveforms/{station}/{channel}")
+
+
+
+    dupl_evID = 0
+    n=0
+
+    while n <= len(wf_filelist): ## not sure a better way to execute this? But it works
+
+        try:   #catch generator "stop iteration" error
+
+
+            #these all defined in generator at top of script
+            data, evID, n = next(gen_wf)
+            print(n,evID)
+            # if evID not in group, add dataset to wf group
+            if evID not in channel_group:
+                channel_group.create_dataset(name= evID, data=data)
+                evID_keep.append(int(evID))
+            elif evID in channel_group:
+                dupl_evID += 1
+
+        except StopIteration: #handle generator error
+            break
+
+
+    sampling_rate = wf_test[0].stats.sampling_rate
+    # instr_response =
+    station_info = f"{wf_test[0].stats.network}.{wf_test[0].stats.station}.{wf_test[0].stats.location}.{wf_test[0].stats.channel}."
+    calib = wf_test[0].stats.calib
+    _format = wf_test[0].stats._format
+
+
+    processing_group = h5file.create_group(f"{station}/processing_info")
+
+
+    processing_group.create_dataset(name= "sampling_rate_Hz", data=sampling_rate)#,dtype='S')
+    processing_group.create_dataset(name= "station_info", data=station_info)
+    processing_group.create_dataset(name= "calibration", data=calib)#,dtype='S')
+    processing_group.create_dataset(name= "orig_formata", data=_format)#,dtype='S')
+    # processing_group.create_dataset(name= "instr_response", data=instr_response)#,dtype='S')
+    processing_group.create_dataset(name= "lenData", data=lenData)#,dtype='S')
+
+
+
+
+
+
+print(dupl_evID, ' duplicate events found and avoided')
+print(n- dupl_evID, ' waveforms loaded')
+
+
+
+#%%
+
+
+cat_keep_wf = cat[cat['event_ID'].isin(evID_keep)]
+
+if os.path.exists(wf_cat_out):
+    os.remove(wf_cat_out)
+
+
+cat_keep_wf.to_csv(wf_cat_out)
+
+print(len(cat_keep_wf), ' events in wf catalog')
+
+
+#%%
---
+++ 3_runSpecUFEx.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 19 12:02:51 2021
+
+@author: theresasawi
+"""
+
+import h5py
+import numpy as np
+import sys
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+sys.path.append('functions/')
+from setParams import setParams
+# from generators import gen_sgram_QC
+
+import tables
+tables.file._open_files.close_all()
+from specufex import BayesianNonparametricNMF, BayesianHMM
+
+
+#%% load project variables: names and paths
+
+key = sys.argv[1]
+
+# key = 'Parkfield_Repeaters'
+
+NbNMFsteps = 50000
+NbHMMsteps = 100000
+
+
+
+
+#%%
+### do not change these ###
+
+pathProj, pathCat, pathWF, network, station, channel, channel_ID, filetype, cat_columns = setParams(key)
+
+
+
+dataH5_name = f'data_{key}.hdf5'
+dataH5_path = pathProj + '/H5files/' + dataH5_name
+SpecUFEx_H5_name = f'SpecUFEx_{key}.hdf5'
+SpecUFEx_H5_path = pathProj + '/H5files/' + SpecUFEx_H5_name
+sgramMatOut = pathProj + 'matSgrams/'## for testing
+pathWf_cat  = pathProj + 'wf_cat_out.csv'
+pathSgram_cat = pathProj + f'sgram_cat_out_{key}.csv'
+
+sgram_cat = pd.read_csv(pathSgram_cat)
+
+
+
+#%% get spectrograms from H5
+
+
+X= []
+H5=True
+
+if H5:
+    with h5py.File(SpecUFEx_H5_path,'a') as fileLoad:
+        for evID in fileLoad['spectrograms']:
+            specMat = fileLoad['spectrograms'].get(evID)[:]
+            Xis = specMat
+            X.append(Xis)
+
+X = np.array(X)
+
+#%%
+nmf = BayesianNonparametricNMF()
+nmf.fit(X, NbSteps=NbNMFsteps,verbose=1)
+As, Xpwrs = nmf.transform(X)
+
+#%%
+
+hmm = BayesianHMM()
+hmm.fit(As, nmf.EW, NbSteps=NbHMMsteps)
+fingerprints, As, Ppis = hmm.transform(As)
+
+#%%
+plt.imshow(fingerprints[0])
+#%%
+#%%
+#%%
+
+# =============================================================================
+# save output to H5
+# =============================================================================
+with h5py.File(SpecUFEx_H5_path,'a') as fileLoad:
+
+
+    ##fingerprints are top folder
+    if 'fingerprints' in fileLoad.keys():
+        del fileLoad["fingerprints"]
+
+    fp_group = fileLoad.create_group('fingerprints')
+
+    out_group               = fileLoad.create_group("SpecUFEX_output")
+    
+    ACM_group               = fileLoad.create_group("SpecUFEX_output/ACM")
+    STM_group               = fileLoad.create_group("SpecUFEX_output/STM")
+    gain_group              = fileLoad.create_group("SpecUFEX_output/ACM_gain")
+
+
+    
+    for i, evID in enumerate(fileLoad['spectrograms']):
+        fp_group.create_dataset(name= evID, data=fingerprints[i])
+        # ACM_group.create_dataset(name=evID,data=As[i]) #ACM
+        # STM_group.create_dataset(name=evID,data=gam[i]) #STM
+
+
+    W_group                      = fileLoad.create_group("SpecUFEX_output/W")
+    gain_group                   = fileLoad.create_group("SpecUFEX_output/gain")
+    EB_group                     = fileLoad.create_group("SpecUFEX_output/EB")
+    RMM_group                    = fileLoad.create_group("SpecUFEX_output/RMM")
+
+
+    
+    W_group.create_dataset(name='W',data=nmf.EW)
+    EB_group.create_dataset(name=evID,data=hmm.EB)
+    # RMM_group.create_dataset(name=evID,data=RMM)
+    gain_group.create_dataset(name='gain',data=nmf.gain) #same for all data               
+
+#%%
+#%%
+
+
+
+#%%
+
+
+
+#%%
